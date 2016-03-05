e2359354df1a0dabb7e6217cebd5dc2ef5a228df
bggd <gameovera@live.com>
2016-03-05 22:36:11 +0900

    Add build_config.rb

---
+++ minigame_build_config.rb
@@ -0,0 +1,26 @@
+MRuby::Build.new do |conf|
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+  end
+
+  conf.gembox 'full-core'
+end
+
+
+MRuby::Build.new('test') do |conf|
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+  end
+
+  enable_debug
+  conf.enable_bintest
+  conf.enable_test
+
+  conf.gembox 'full-core'
+end
