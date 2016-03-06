d208dc1c12203ddb29f965f43717017d71b1d0f8
bggd <gameovera@live.com>
2016-03-06 11:57:10 +0900

    Update appveyor.yml

--- minigame_build_config.rb
+++ minigame_build_config.rb
@@ -6,7 +6,23 @@ MRuby::Build.new do |conf|
     toolchain :gcc
   end
 
+  conf.cc do |cc|
+    cc.flags = %w(/c /nologo /W3 /we4013 /MT /O2 /D_CRT_SECURE_NO_WARNINGS)
+  end
+
+  conf.linker do |linker|
+    # ignore many warning for linking Allegro5
+    linker.link_options << '/ignore:4099'
+  end
+
   conf.gembox 'full-core'
+
+  conf.gem 'mruby-minigame' do |g|
+    g.cc.defines << 'ALLEGRO_STATICLINK'
+    g.cc.include_paths << ENV['A5_INC_DIR']
+    g.linker.library_paths << ENV['A5_LIB_DIR'] << ENV['A5DEPS_LIB_DIR']
+    g.linker.libraries = %w(allegro_monolith-static dumb FLAC ogg vorbis vorbisfile freetype jpeg libpng16 zlib opengl32 user32 ole32 gdi32 winmm psapi shell32 shlwapi)
+  end
 end
 
 
