b81a48b689fb79d2ed784b29f0d5a3f2f4122f89
bggd <gameovera@live.com>
2016-03-06 22:08:28 +0900

    Update build_config.rb

--- minigame_build_config.rb
+++ minigame_build_config.rb
@@ -23,22 +23,9 @@ MRuby::Build.new do |conf|
     g.linker.library_paths << ENV['A5_LIB_DIR'] << ENV['A5DEPS_LIB_DIR']
     g.linker.libraries = %w(allegro_monolith-static dumb FLAC ogg vorbis vorbisfile freetype jpeg libpng16 zlib opengl32 user32 ole32 gdi32 winmm psapi shell32 shlwapi)
   end
-end
 
-
-MRuby::Build.new('test') do |conf|
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
-
-  enable_debug
   conf.enable_bintest
   conf.enable_test
-
-  conf.gembox 'full-core'
 
   conf.file_separator = '/'
 end
